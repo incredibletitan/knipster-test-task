92e810cf9a755547632428ca8363c54630d08e45
Yuriy Stos <100sbsh@gmail.com>
2017-03-10 14:54:45 +0200

    Added default value to migrations

--- database/migrations/2017_03_10_103007_create_user_table.php
+++ database/migrations/2017_03_10_103007_create_user_table.php
@@ -16,13 +16,13 @@ class CreateUserTable extends Migration
         Schema::create('user', function (Blueprint $table) {
             $table->increments('id');
             $table->string('email')->unique();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('gender');
-            $table->string('country');
-            $table->tinyInteger('balance_percentage');
-            $table->float('balance');
-            $table->float('bonus_balance');
+            $table->string('first_name')->default('');
+            $table->string('last_name')->default('');
+            $table->string('gender')->default('');
+            $table->string('country')->default('');
+            $table->tinyInteger('balance_percentage')->default(0);
+            $table->float('balance')->default(0);
+            $table->float('bonus_balance')->default(0);
             $table->timestamps();
         });
     }
