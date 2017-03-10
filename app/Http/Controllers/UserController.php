315e63e15835827512f76bb36a1af34d9d57a1c9
Yuriy Stos <100sbsh@gmail.com>
2017-03-10 13:57:24 +0200

    Added StoreUser request validation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUser;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -12,7 +13,7 @@ class UserController extends Controller
      * @param Request $request
      * @return \Illuminate\Http\JsonResponse
      */
-    public function store(Request $request)
+    public function store(StoreUser $request)
     {
         return response()->json([
             'ololo'
